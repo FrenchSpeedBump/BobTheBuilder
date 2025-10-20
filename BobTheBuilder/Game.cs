10898398f1f5650a7bc9e4e7e3bd3b09e3da6af5
FrenchSpeedBump <alex.zalanyi21@gmail.com>
2025-10-20 12:27:29 +0200

    added rooms

--- BobTheBuilder/Game.cs
+++ BobTheBuilder/Game.cs
@@ -17,8 +17,18 @@
             Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
             Room? pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
             Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
-            Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
-
+            //Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
+            Room? house = new("House", "This is where we are going to build a house :)");
+            Room? street_1 = new("Street_1", "A street leading to our house going into the city. Nothing but nature around us.");
+            Room? street_main = new("Street_Main", "The main street ouf our town. The street has an office building on one side and a bank on the other");
+            Room? street_north = new("Street_North", "The north street has two shops: Bob's Materials and the Magic Tool Shop.");
+            Room? office = new("Office Building", "When you step into this old office building you are met with three construction company signs: Best Build, Big Build and Small Build");
+            Room? bank = new("Bank", "A nice empty building where at the other side of the room a young teller is smiling at you");
+            Room? materials = new("Bob's Materials", "Rows and rows of construction materials");
+            Room? tools = new("Magic Tool Shop", "A nice old man is very happy to tell you everything about a hammer");
+            Room? cons_1 = new("Best Build", "Constructions");
+            Room? cons_2 = new("Big Build", "Constructions");
+            Room? cons_3 = new("Small Build", "Constructions");
             outside.SetExits(null, theatre, lab, pub); // North, East, South, West
 
             theatre.SetExit("west", outside);
