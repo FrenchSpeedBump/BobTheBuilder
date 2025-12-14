e9f35131ff139bf7843e632e1e6d07c77a0e3e1a
FrenchSpeedBump <alex.zalanyi21@gmail.com>
2025-12-14 16:25:48 +0100

    ui change

--- BobTheBuilder/Presentation/UI/StatisticsUI.cs
+++ BobTheBuilder/Presentation/UI/StatisticsUI.cs
@@ -4,7 +4,7 @@ namespace BobTheBuilder
     {
         public static void DisplayStats(Statistics statistics, int day)
         {
-            Console.WriteLine("==============|Statistics for Day " + day + "|==============");
+            Console.WriteLine("============|Statistics for Day " + day + "|============");
             Console.WriteLine("Average Sustainability: " + statistics.GetAverageSustainability());
             Console.WriteLine("Average Quality: " + statistics.GetAverageQuality());
             Console.WriteLine("Total Money Spent: " + statistics.GetTotalMoneySpent());
